d4e3182245be72dad2b5c9dfbe96c4b50c0772d0
Tan Bin Ming <bin.ming@gmail.com>
2022-09-13 20:34:07 +0800

    changed tensorflow package requirement to cpu version

--- mlproject/semantic_segmentation.py
+++ mlproject/semantic_segmentation.py
@@ -6,7 +6,6 @@ from matplotlib import pyplot as plt
 from patchify import patchify
 from PIL import Image
 import segmentation_models as sm
-from tensorflow.keras.metrics import MeanIoU
 
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 scaler = MinMaxScaler()
